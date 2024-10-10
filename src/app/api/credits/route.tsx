2ec5c957a5faa42c42e0faa40e8151ec081f108b
Guilherme Garcia <garciaguig@gmail.com>
2024-10-09 23:36:17 -0300

    feat: Implements GET credits API route

---
+++ src/app/api/credits/route.tsx
@@ -0,0 +1,52 @@
+import prisma from '@/lib/prisma'
+import { NotFoundException } from '@/utils/exceptions'
+import HttpException from '@/utils/exceptions/HttpException'
+import { NextResponse } from 'next/server'
+
+export const dynamic = 'force-dynamic'
+
+export async function GET() {
+  let credits
+
+  try {
+    credits = await prisma.credit.findMany({
+      include: {
+        seller: { select: { name: true } },
+      },
+      where: {
+        currentAmount: { gt: 0 },
+      },
+    })
+
+    if (!credits) {
+      throw new NotFoundException('Créditos não encontrados')
+    } else {
+      credits = credits.map((credit) => {
+        return {
+          ...credit,
+          seller: credit.seller.name,
+        }
+      })
+    }
+
+    return NextResponse.json(
+      {
+        message: 'Créditos encontrados',
+        data: credits,
+      },
+      { status: 200 },
+    )
+  } catch (error: unknown) {
+    if (error instanceof HttpException) {
+      const { message, status } = error
+      console.error(message, status)
+
+      return NextResponse.json({ message }, { status })
+    } else {
+      const message = 'Erro inesperado ao buscar créditos'
+      console.error(message, error)
+
+      return NextResponse.json({ message }, { status: 500 })
+    }
+  }
+}

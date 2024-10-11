5896bc49a373f239dcb0c5565aafb9f30a394268
Guilherme Garcia <garciaguig@gmail.com>
2024-10-10 21:49:31 -0300

    chore: Adds new types for 'Products' shop page

--- src/app/shop/products/_utils/types.ts
+++ src/app/shop/products/_utils/types.ts
@@ -3,3 +3,11 @@ import { CustomCredit } from '@/types/global'
 export type ShopCardProps = {
   credit: CustomCredit
 }
+
+export type CartCardProps = {
+  credit: CustomCredit
+}
+
+export type CartActionsProps = {
+  totalPrice: string
+}

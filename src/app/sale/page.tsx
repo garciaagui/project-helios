db367f5b3e05e3de351b5f4fe5ce25df9269b116
Guilherme Garcia <garciaguig@gmail.com>
2024-11-20 20:00:39 -0300

    feat: Implements navigation menu in 'sale' page

--- src/app/sale/page.tsx
+++ src/app/sale/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { NavigationMenu } from '@/components/navigation-menu'
 import { Button } from '@/components/ui/button'
 import {
   Form,
@@ -62,86 +63,91 @@ export default function Sale() {
   }
 
   return (
-    <main className="mx-auto my-10 flex w-1/2 flex-col gap-y-12">
-      <div className="flex flex-col">
-        <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">Venda</h2>
-        <p className="[&:not(:first-child)] leading-7">
-          Preencha as informações e coloque seus créditos à venda.
-        </p>
-      </div>
+    <>
+      <NavigationMenu />
+      <main className="mx-auto my-10 flex w-1/2 flex-col gap-y-12">
+        <div className="flex flex-col">
+          <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+            Venda
+          </h2>
+          <p className="[&:not(:first-child)] leading-7">
+            Preencha as informações e coloque seus créditos à venda.
+          </p>
+        </div>
 
-      <Form {...form}>
-        <form
-          onSubmit={handleSubmit(finishSale)}
-          className="flex w-full flex-col gap-8"
-          id="sale-form"
-        >
-          <FormField
-            control={control}
-            name="creditAmount"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Quantidade de créditos</FormLabel>
-                <FormControl>
-                  <Input
-                    type="number"
-                    {...field}
-                    onChange={(e) => {
-                      const inputValue = e.target.value
-                      field.onChange(Number(inputValue))
-                    }}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <Form {...form}>
+          <form
+            onSubmit={handleSubmit(finishSale)}
+            className="flex w-full flex-col gap-8"
+            id="sale-form"
+          >
+            <FormField
+              control={control}
+              name="creditAmount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Quantidade de créditos</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      {...field}
+                      onChange={(e) => {
+                        const inputValue = e.target.value
+                        field.onChange(Number(inputValue))
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            control={control}
-            name="unitPrice"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Valor unitário (R$)</FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    {...field}
-                    value={unitPrice}
-                    onChange={handleUnitPriceChange}
-                  />
-                </FormControl>
-                <FormDescription>Valor mínimo: R$ 0,10</FormDescription>
-                <FormDescription>
-                  Nossa plataforma cobra uma taxa de 10% sobre o valor unitário
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormField
+              control={control}
+              name="unitPrice"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Valor unitário (R$)</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      {...field}
+                      value={unitPrice}
+                      onChange={handleUnitPriceChange}
+                    />
+                  </FormControl>
+                  <FormDescription>Valor mínimo: R$ 0,10</FormDescription>
+                  <FormDescription>
+                    Nossa plataforma cobra uma taxa de 10% sobre o valor unitário
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            control={control}
-            name="creditProofFile"
-            render={() => (
-              <FormItem>
-                <FormLabel>Arquivo</FormLabel>
-                <FormControl>
-                  <Input type="file" onChange={handleFileChange} />
-                </FormControl>
-                <FormDescription>
-                  Insira o documento que comprove os créditos que serão vendidos (formato PDF)
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" onClick={() => finishSale()}>
-            Finalizar
-          </Button>
-        </form>
-      </Form>
-      {isSubmitSuccessful ? <SaleCompletedDialog /> : ''}
-    </main>
+            <FormField
+              control={control}
+              name="creditProofFile"
+              render={() => (
+                <FormItem>
+                  <FormLabel>Arquivo</FormLabel>
+                  <FormControl>
+                    <Input type="file" onChange={handleFileChange} />
+                  </FormControl>
+                  <FormDescription>
+                    Insira o documento que comprove os créditos que serão vendidos (formato PDF)
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" onClick={() => finishSale()}>
+              Finalizar
+            </Button>
+          </form>
+        </Form>
+        {isSubmitSuccessful ? <SaleCompletedDialog /> : ''}
+      </main>
+    </>
   )
 }

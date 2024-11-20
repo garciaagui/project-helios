aca0f8c158d1c2c1d75886231852d01e35aeccb0
Guilherme Garcia <garciaguig@gmail.com>
2024-11-20 20:40:01 -0300

    feat: Implements new structure for 'home' page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,44 @@
 'use client'
 
+import { NavigationMenu } from '@/components/navigation-menu'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import Layout from '@/components/ui/layout'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <Layout>
+    <>
+      <NavigationMenu />
       <div className="z-10 flex flex-col items-center space-y-8">
-        {/* Banner */}
-        <Card className="w-full rounded-lg bg-cyan-200/40 text-center shadow-lg">
+        <Card className="mt-20 w-1/2 rounded-lg bg-secondary text-center shadow-lg">
           <CardHeader>
-            <CardTitle className="mb-4 text-3xl font-bold text-gray-800">
+            <CardTitle className="mb-4 text-3xl font-bold">
               Conduzindo o futuro da energia sustentável
             </CardTitle>
           </CardHeader>
           <CardContent>
-            <p className="text-lg text-gray-600">
+            <p className="text-lg">
               O setor de energias renováveis tem crescido devido à demanda por fontes sustentáveis e
               à necessidade de reduzir o impacto ambiental. Sistemas fotovoltaicos permitem a
               geração de energia em pequena e média escala, promovendo a comercialização de
               excedentes.
             </p>
-            <p className="mt-4 text-lg text-gray-600">
+            <p className="mt-4 text-lg">
               Nosso principal objetivo é a comercialização de créditos excedentes de energia solar,
               promovendo o acesso à energia limpa.
             </p>
           </CardContent>
         </Card>
 
-        {/* Botões de Ação */}
         <div className="flex space-x-4">
-          <Button size={'lg'} className="bg-[#F7D560] text-gray-800 hover:bg-[#f7c64d]">
-            <Link href={'/shop/products'}>Quero Comprar</Link>
+          <Button size={'lg'}>
+            <Link href={'/shop/products'}>Quero comprar</Link>
           </Button>
-          <Button size={'lg'} className="bg-[#F7D560] text-gray-800 hover:bg-[#f7c64d]">
-            <Link href={'/sale'}>Quero Vender</Link>
+          <Button size={'lg'}>
+            <Link href={'/sale'}>Quero vender</Link>
           </Button>
         </div>
       </div>
-    </Layout>
+    </>
   )
 }
